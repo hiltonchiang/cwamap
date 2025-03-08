4b507645d4999ba65d28c19921b642bcf1825a55
hiltonchang <hiltonchiang@gmail.com>
2025-03-09 05:35:10 +0800

    .github add

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
+const output = process.env.EXPORT ? 'export' : undefined
+const basePath = process.env.BASE_PATH || undefined
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output,
+  basePath
+}
 
 export default nextConfig;
